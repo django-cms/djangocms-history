cc24dd32379f9a4c302cddf7584b0ad79f3f725c
Paulo Alvarado <commonzenpython@gmail.com>
2017-01-23 07:11:02 -0500

    Fixed #6 -- Missing package data on pip install

--- setup.py
+++ setup.py
@@ -3,9 +3,17 @@ from setuptools import find_packages, setup
 import djangocms_history
 
 
+INSTALL_REQUIREMENTS = [
+    'Django>=1.8,<1.11',
+    # TODO: Un-comment once cms is released
+    # 'django-cms>=3.4.2',
+]
+
+
 setup(
     name='djangocms-history',
     packages=find_packages(),
+    include_package_data=True,
     version=djangocms_history.__version__,
     description=djangocms_history.__doc__,
     long_description=open('README.rst').read(),
@@ -16,6 +24,7 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Software Development'
     ],
+    install_requires=INSTALL_REQUIREMENTS,
     author='Divio AG',
     author_email='info@divio.ch',
     url='http://github.com/divio/djangocms-history',
