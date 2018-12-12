9a1dcb7d705212305c59e7e54087ebeb2e31490d
Angelo Dini <angelo.dini@divio.ch>
2018-12-12 14:36:39 +0100

    added test framework

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -2,9 +2,14 @@
 Changelog
 =========
 
+1.0.0 (unreleased)
+==================
+
+* Added support for Django 2.0 and 2.1
+
 
 0.6.0 (2018-11-14)
 ==================
 
-* Added support for Django 1.11, 2.0 and 2.1
+* Added support for Django 1.11
 * Removed support for Django<1.11
--- setup.py
+++ setup.py
@@ -1,32 +1,46 @@
 from setuptools import find_packages, setup
 
-import djangocms_history
+from djangocms_history import __version__
 
 
-INSTALL_REQUIREMENTS = [
-    'Django>=1.11,<2.2',
+REQUIREMENTS = [
     'django-cms>=3.4.5',
-    'django-sekizai>=0.7',
+]
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Software Development :: Libraries :: Python Modules',
 ]
 
 
 setup(
     name='djangocms-history',
-    packages=find_packages(),
-    include_package_data=True,
-    version=djangocms_history.__version__,
-    description=djangocms_history.__doc__,
-    long_description=open('README.rst').read(),
-    classifiers=[
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development'
-    ],
-    install_requires=INSTALL_REQUIREMENTS,
+    version=__version__,
+    description='Adds undo/redo functionality to django CMS',
     author='Divio AG',
     author_email='info@divio.ch',
-    url='http://github.com/divio/djangocms-history',
+    url='https://github.com/divio/djangocms-history',
     license='BSD',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    test_suite='tests.settings.run',
 )
