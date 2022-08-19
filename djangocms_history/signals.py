d2e19b7e1e21183e9fb9a167140e0485186d5442
Angelo Dini <angelo.dini@divio.ch>
2022-08-19 15:32:48 +0200

    Bumped version to 2.1.0 (#42)

--- djangocms_history/signals.py
+++ djangocms_history/signals.py
@@ -1,16 +1,6 @@
 from django.dispatch import Signal
 
 
-post_operation_undo = Signal(
-    providing_args=[
-        "operation",
-        "actions",
-    ]
-)
+post_operation_undo = Signal("operation actions")
 
-post_operation_redo = Signal(
-    providing_args=[
-        "operation",
-        "actions",
-    ]
-)
+post_operation_redo = Signal("operation actions")
