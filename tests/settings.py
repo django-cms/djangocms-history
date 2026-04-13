772a6ae29ad3e5461300c44022418f745e1b7ba1
daniel <contact@dnlzrgz.com>
2026-04-13 10:33:37 +0200

    test: migrate test runner to pytest

--- tests/settings.py
+++ tests/settings.py
@@ -1,26 +1,77 @@
-#!/usr/bin/env python
-HELPER_SETTINGS = {
-    "INSTALLED_APPS": [
-        "djangocms_history",
-    ],
-    "CMS_LANGUAGES": {
-        1: [
-            {
-                "code": "en",
-                "name": "English",
-            }
-        ]
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = "fake-key"
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "django.contrib.sitemaps",
+    "cms",
+    "menus",
+    "treebeard",
+    "djangocms_history",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "cms.middleware.user.CurrentUserMiddleware",
+    "cms.middleware.page.CurrentPageMiddleware",
+    "cms.middleware.toolbar.ToolbarMiddleware",
+    "cms.middleware.language.LanguageCookieMiddleware",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "cms.context_processors.cms_settings",
+            ],
+        },
     },
-    "LANGUAGE_CODE": "en",
-    "ALLOWED_HOSTS": ["localhost"],
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
 }
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-def run():
-    from app_helper import runner
+LANGUAGE_CODE = "en"
 
-    runner.cms("djangocms_history")
+CMS_LANGUAGES = {1: [{"code": "en", "name": "English"}]}
 
+USE_TZ = True
 
-if __name__ == "__main__":
-    run()
+TIME_ZONE = "UTC"
+
+SITE_ID = 1
+
+STATIC_URL = "/static/"
